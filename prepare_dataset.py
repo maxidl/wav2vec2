b17c4d20902650171a221618b525e3eba578fad7
Maximilian Idahl <idahl@cubone.localdomain>
2021-03-25 21:12:35 +0100

    initial code

---
+++ prepare_dataset.py
@@ -0,0 +1,160 @@
+import json
+import re
+from pathlib import Path
+
+from tqdm.auto import tqdm
+import torch
+import torchaudio
+import pandas as pd
+import pyarrow.parquet as pq
+
+import datasets
+from transformers import (
+    HfArgumentParser,
+    TrainingArguments,
+    Wav2Vec2CTCTokenizer,
+    Wav2Vec2FeatureExtractor,
+    Wav2Vec2Processor,
+)
+
+from argument_classes import ModelArguments, DataTrainingArguments
+
+args_file = './args.json'
+parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+model_args, data_args, training_args = parser.parse_json_file(args_file)
+
+# increasing number of threads for torchaudio resample
+torch.set_num_threads(data_args.preprocessing_num_workers)
+
+# Get the datasets:
+train_dataset = datasets.load_dataset(
+    "common_voice", data_args.dataset_config_name, split=data_args.train_split_name
+)
+eval_dataset = datasets.load_dataset("common_voice", data_args.dataset_config_name, split="test")
+
+# Create and save tokenizer
+chars_to_ignore_regex = f'[{"".join(data_args.chars_to_ignore)}]'
+
+def remove_special_characters(batch):
+    batch["text"] = re.sub(chars_to_ignore_regex, "", batch["sentence"]).lower() + " "
+    return batch
+
+train_dataset = train_dataset.map(remove_special_characters, remove_columns=["sentence"], keep_in_memory=True, num_proc=data_args.preprocessing_num_workers)
+eval_dataset = eval_dataset.map(remove_special_characters, remove_columns=["sentence"], keep_in_memory=True, num_proc=data_args.preprocessing_num_workers)
+
+def extract_all_chars(batch):
+    all_text = " ".join(batch["text"])
+    vocab = list(set(all_text))
+    return {"vocab": [vocab], "all_text": [all_text]}
+
+vocab_train = train_dataset.map(
+    extract_all_chars,
+    batched=True,
+    batch_size=-1,
+    keep_in_memory=True,
+    remove_columns=train_dataset.column_names,
+)
+vocab_test = train_dataset.map(
+    extract_all_chars,
+    batched=True,
+    batch_size=-1,
+    keep_in_memory=True,
+    remove_columns=eval_dataset.column_names,
+)
+
+vocab_list = list(set(vocab_train["vocab"][0]) | set(vocab_test["vocab"][0]))
+vocab_dict = {v: k for k, v in enumerate(vocab_list)}
+vocab_dict["|"] = vocab_dict[" "]
+del vocab_dict[" "]
+vocab_dict["[UNK]"] = len(vocab_dict)
+vocab_dict["[PAD]"] = len(vocab_dict)
+
+with open("vocab.json", "w") as vocab_file:
+    json.dump(vocab_dict, vocab_file)
+
+if data_args.max_train_samples is not None:
+    train_dataset = train_dataset.select(range(data_args.max_train_samples))
+
+if data_args.max_val_samples is not None:
+    eval_dataset = eval_dataset.select(range(data_args.max_val_samples))
+
+# Load pretrained tokenizer & create processor
+tokenizer = Wav2Vec2CTCTokenizer(
+    "vocab.json",
+    unk_token="[UNK]",
+    pad_token="[PAD]",
+    word_delimiter_token="|",
+)
+feature_extractor = Wav2Vec2FeatureExtractor(
+    feature_size=1, sampling_rate=16_000, padding_value=0.0, do_normalize=True, return_attention_mask=True
+)
+processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
+
+# =======================================================
+# The following part is modified to:
+#   - load resample and save audio files into raw tensors
+#   - process labels as before, but do not process the audio tensors (these will be processed during training)
+#   - save datasets containing the paths and labels to disk
+
+# load and resample audio, save as raw tensors
+resampler = torchaudio.transforms.Resample(48_000, 16_000)
+resampled_data_dir = Path('./resampled')
+resampled_data_dir.mkdir(exist_ok=True)
+
+def load_resample_save(f):
+    f = Path(f)
+    new_path = resampled_data_dir / f'{f.stem}_resampled16k.pt'
+    if not new_path.exists():
+        speech_array, sampling_rate = torchaudio.load(f)
+        speech_array_resampled = resampler(speech_array)
+        input_values = processor(speech_array_resampled, sampling_rate=16_000).input_values
+        torch.save(input_values, new_path)
+    return str(new_path)
+
+print('load resample save')
+new_train_paths = [load_resample_save(f) for f in tqdm(train_dataset['path'], miniters=100, desc='train')]
+new_eval_paths = [load_resample_save(f) for f in tqdm(eval_dataset['path'], miniters=100, desc='eval')]
+
+# update paths and sampling rate
+train_dataset = train_dataset.map(
+    lambda x: {'path': new_train_paths, 'sampling_rate':[16_000] * len(train_dataset), 'target_text': x['text']},
+    batched=True,
+    batch_size=-1,
+    keep_in_memory=True,
+    remove_columns=train_dataset.column_names,
+)
+eval_dataset = eval_dataset.map(
+    lambda x: {'path': new_eval_paths, 'sampling_rate':[16_000] * len(eval_dataset), 'target_text': x['text']},
+    batched=True,
+    batch_size=-1,
+    keep_in_memory=True,
+    remove_columns=eval_dataset.column_names,
+)
+
+# tokenize targets
+def tokenize_targets(batch):
+    # Setup the processor for targets
+    with processor.as_target_processor():
+        batch["labels"] = processor(batch["target_text"]).input_ids
+    return batch
+
+print('preparing dataset: train')
+train_dataset = train_dataset.map(
+    tokenize_targets,
+    remove_columns=[col for col in train_dataset.column_names if col != 'path'],
+    batch_size=training_args.per_device_train_batch_size,
+    batched=True,
+    num_proc=data_args.preprocessing_num_workers,
+)
+print('preparing dataset: eval')
+eval_dataset = eval_dataset.map(
+    tokenize_targets,
+    remove_columns=[col for col in eval_dataset.column_names if col != 'path'],
+    batch_size=training_args.per_device_train_batch_size,
+    batched=True,
+    num_proc=data_args.preprocessing_num_workers,
+)
+
+# # save for disk, ready for training
+pq.write_table(train_dataset.data, f'./{data_args.dataset_config_name}.train.parquet')
+pq.write_table(eval_dataset.data, f'./{data_args.dataset_config_name}.eval.parquet')
---
+++ run_finetuning.py
@@ -0,0 +1,345 @@
+import logging
+import os
+import sys
+from pathlib import Path
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union
+from multiprocessing import Pool
+
+from tqdm.auto import tqdm
+import numpy as np
+import torch
+from torch import nn
+from torch.cuda.amp import autocast
+import pandas as pd
+import pyarrow.parquet as pq
+
+import datasets
+import transformers
+from transformers import (
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    Wav2Vec2CTCTokenizer,
+    Wav2Vec2FeatureExtractor,
+    Wav2Vec2ForCTC,
+    Wav2Vec2Processor,
+    set_seed,
+)
+
+from transformers.trainer_utils import get_last_checkpoint, is_main_process
+
+from argument_classes import ModelArguments, DataTrainingArguments
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class DataCollatorCTCWithPadding:
+    """
+    Data collator that will dynamically pad the inputs received.
+    Args:
+        processor (:class:`~transformers.Wav2Vec2Processor`)
+            The processor used for proccessing the data.
+        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`True`):
+            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
+            among:
+            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
+              sequence if provided).
+            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
+              maximum acceptable input length for the model if that argument is not provided.
+            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
+              different lengths).
+        max_length (:obj:`int`, `optional`):
+            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
+        max_length_labels (:obj:`int`, `optional`):
+            Maximum length of the ``labels`` returned list and optionally padding length (see above).
+        pad_to_multiple_of (:obj:`int`, `optional`):
+            If set will pad the sequence to a multiple of the provided value.
+            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
+            7.5 (Volta).
+    """
+
+    processor: Wav2Vec2Processor
+    padding: Union[bool, str] = True
+    max_length: Optional[int] = None
+    max_length_labels: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    pad_to_multiple_of_labels: Optional[int] = None
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # split inputs and labels since they have to be of different lenghts and need
+        # different padding methods
+        input_features = [{"input_values": feature["input_values"]} for feature in features]
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
+
+        batch = self.processor.pad(
+            input_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+        with self.processor.as_target_processor():
+            labels_batch = self.processor.pad(
+                label_features,
+                padding=self.padding,
+                max_length=self.max_length_labels,
+                pad_to_multiple_of=self.pad_to_multiple_of_labels,
+                return_tensors="pt",
+            )
+
+        # replace padding with -100 to ignore loss correctly
+        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+
+        batch["labels"] = labels
+
+        return batch
+
+
+class CTCTrainer(Trainer):
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        """
+        Perform a training step on a batch of inputs.
+
+        Subclass and override to inject custom behavior.
+
+        Args:
+            model (:obj:`nn.Module`):
+                The model to train.
+            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
+
+        Return:
+            :obj:`torch.Tensor`: The tensor with training loss on this batch.
+        """
+
+        model.train()
+        inputs = self._prepare_inputs(inputs)
+
+        if self.use_amp:
+            with autocast():
+                loss = self.compute_loss(model, inputs)
+        else:
+            loss = self.compute_loss(model, inputs)
+
+        if self.args.n_gpu > 1:
+            if model.module.config.ctc_loss_reduction == "mean":
+                loss = loss.mean()
+            elif model.module.config.ctc_loss_reduction == "sum":
+                loss = loss.sum() / (inputs["labels"] >= 0).sum()
+            else:
+                raise ValueError(f"{model.config.ctc_loss_reduction} is not valid. Choose one of ['mean', 'sum']")
+
+        if self.args.gradient_accumulation_steps > 1:
+            loss = loss / self.args.gradient_accumulation_steps
+
+        if self.use_amp:
+            self.scaler.scale(loss).backward()
+        # elif self.use_apex:
+        #     with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+        #         scaled_loss.backward()
+        elif self.deepspeed:
+            self.deepspeed.backward(loss)
+        else:
+            loss.backward()
+
+        return loss.detach()
+
+
+def load_speech(f):
+    return torch.load(f).squeeze().numpy()
+
+
+def main():
+
+    args_file = './args.json'
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_json_file(args_file)
+
+    torch.set_num_threads(data_args.preprocessing_num_workers)
+
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    logger.setLevel(logging.INFO if is_main_process(training_args.local_rank) else logging.WARN)
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
+    # Set the verbosity to info of the Transformers logger (on main process only):
+    if is_main_process(training_args.local_rank):
+        transformers.utils.logging.set_verbosity_info()
+    logger.info("Training/evaluation parameters %s", training_args)
+
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+
+    # Get the datasets:
+    class CustomWav2Vec2Dataset(torch.utils.data.Dataset):
+
+        def __init__(self, split='train', in_memory=True) -> None:
+            super().__init__()
+            assert split in {'train', 'eval'}
+            self.split = split
+            self.in_memory = in_memory
+            self.path = Path(f'./{data_args.dataset_config_name}.{split}.parquet')
+            df = pd.read_parquet(self.path)
+            self.labels = df['labels'].tolist()
+            self.paths = df['path'].tolist()
+
+            if self.in_memory:
+                with Pool(data_args.preprocessing_num_workers) as p:
+                    self.input_values = list(tqdm(p.imap(load_speech, self.paths), miniters=100, desc=f'loading {self.split} speeches', total=len(self.paths)))
+
+        def __len__(self):
+            return len(self.paths)
+
+        def __getitem__(self, idx):
+            print(idx, self.split)
+            # speech = torch.load(self.paths[idx]).squeeze().numpy()
+            # input_values = processor(speech, sampling_rate=16_000).input_values
+            if self.in_memory:
+                input_values = self.input_values[idx]
+            else:
+                input_values = load_speech(self.paths[idx])
+            label = self.labels[idx]
+            return {'input_values': input_values.tolist()[0], 'labels': label}
+
+
+    train_dataset = CustomWav2Vec2Dataset('train')
+    eval_dataset = CustomWav2Vec2Dataset('eval')
+
+    for a in tqdm(eval_dataset):
+        _ = a
+
+    # Load pretrained model and tokenizer
+    #
+    # Distributed training:
+    # The .from_pretrained methods guarantee that only one local process can concurrently
+    # download model & vocab.
+    tokenizer = Wav2Vec2CTCTokenizer(
+        "vocab.json",
+        unk_token="[UNK]",
+        pad_token="[PAD]",
+        word_delimiter_token="|",
+    )
+    feature_extractor = Wav2Vec2FeatureExtractor(
+        feature_size=1, sampling_rate=16_000, padding_value=0.0, do_normalize=True, return_attention_mask=True
+    )
+    processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
+
+    model = Wav2Vec2ForCTC.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+        activation_dropout=model_args.activation_dropout,
+        attention_dropout=model_args.attention_dropout,
+        hidden_dropout=model_args.hidden_dropout,
+        feat_proj_dropout=model_args.feat_proj_dropout,
+        mask_time_prob=model_args.mask_time_prob,
+        gradient_checkpointing=model_args.gradient_checkpointing,
+        layerdrop=model_args.layerdrop,
+        ctc_loss_reduction="mean",
+        pad_token_id=processor.tokenizer.pad_token_id,
+        vocab_size=len(processor.tokenizer),
+    )
+
+    # Metric
+    wer_metric = datasets.load_metric("wer")
+
+    def compute_metrics(pred):
+        pred_logits = pred.predictions
+        pred_ids = np.argmax(pred_logits, axis=-1)
+
+        pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
+
+        pred_str = processor.batch_decode(pred_ids)
+        # we do not want to group tokens when computing the metrics
+        label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
+
+        wer = wer_metric.compute(predictions=pred_str, references=label_str)
+
+        return {"wer": wer}
+
+    if model_args.freeze_feature_extractor:
+        model.freeze_feature_extractor()
+
+    # Data collator
+    data_collator = DataCollatorCTCWithPadding(processor=processor, padding=True)
+
+    # Initialize our Trainer
+    trainer = CTCTrainer(
+        model=model,
+        data_collator=data_collator,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
+        tokenizer=processor.feature_extractor,
+    )
+
+    # Training
+    if training_args.do_train:
+        if last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        elif os.path.isdir(model_args.model_name_or_path):
+            checkpoint = model_args.model_name_or_path
+        else:
+            checkpoint = None
+
+        print('Training...')
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()
+
+        # save the feature_extractor and the tokenizer
+        if is_main_process(training_args.local_rank):
+            processor.save_pretrained(training_args.output_dir)
+
+        metrics = train_result.metrics
+        max_train_samples = (
+            data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
+        )
+        metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+    # Evaluation
+    results = {}
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = trainer.evaluate()
+        max_val_samples = data_args.max_val_samples if data_args.max_val_samples is not None else len(eval_dataset)
+        metrics["eval_samples"] = min(max_val_samples, len(eval_dataset))
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+    return results
+
+if __name__ == "__main__":
+    main()
